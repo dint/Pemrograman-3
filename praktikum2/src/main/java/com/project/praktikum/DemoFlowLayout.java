6666d31ecb524bd2bdaf90d5e50bbd935cfe82ff
dint <fahrudin1705@gmail.com>
2015-09-25 16:00:17 +0700

    praktikum2

---
+++ praktikum2/src/main/java/com/project/praktikum/DemoFlowLayout.java
@@ -0,0 +1,43 @@
+package com.praktikum.project;
+
+import javax.swing.*;
+import java.awt.FlowLayout;
+
+public class DemoFlowLayout{
+	public static void main(String[] xx){
+		//Beberapa button untuk demo
+		JButton btn1 = new JButton("Tombol 1");
+		JButton btn2 = new JButton("#2");
+		JButton btn3 = new JButton("Tombol Tiga");
+		JButton btn4 = new JButton("Tombol 4 : Panjang");
+		JButton btn5 = new JButton("Btn 5");
+		JButton btn6 = new JButton("No 6");
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		
+		panel.add(btn1);
+		panel.add(btn2);
+		panel.add(btn3);
+		panel.add(btn4);
+		panel.add(btn5);
+		panel.add(btn6);
+		
+		//menginstalkan Frame
+		JFrame fr = new JFrame("Belajar Layout");
+		
+		fr.getContentPane().add(panel);
+		
+		//ukuran frame
+		fr.setSize(800,400);
+		
+		//posisi tengah
+		fr.setLocationRelativeTo(null);
+		
+		//mematikan program bila sudah
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//tampilkan
+		fr.setVisible(true);
+		}
+}
